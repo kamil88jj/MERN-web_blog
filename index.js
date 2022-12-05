666eedb46477d37d2cc5cf1daa6f5039b762531a
Kamil <isaevkamil029@mail.ru>
2022-12-05 13:48:17 +0300

    add tags

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-
+import cors from 'cors';
 
 import mongoose from 'mongoose';
 
@@ -34,6 +34,7 @@ const upload = multer({ storage });
 
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors,  UserController.login);
@@ -47,7 +48,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     });
 });
 
+app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
